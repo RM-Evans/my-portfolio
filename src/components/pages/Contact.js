45082f1b9d13426f3bf3f1611c034743b2921239
Jawsbreaker <evansrichardmichael@gmail.com>
2021-06-11 18:28:46 -0600

    initial commit

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => (
+    <div>
+            <div class="contact-page">
+                <h1 class="contact-header"> Trying to contact me? </h1>
+                <div class="email-container">
+                    <h2> Send me an email</h2>
+                    <a class='email-link' href="mailto:test@gmail.com">test@gmail.com</a>
+                </div>
+            </div>
+    
+    </div>
+
+        );
+
+        export default Contact;
